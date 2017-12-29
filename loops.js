d4db71c748c2e9d493e931d53037773d944bb525
Bekah Hawrot Weigel <rhawrot@gmail.com>
2017-12-29 21:13:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,8 @@
 var array = []
  function forLoop(array){
-for (var i = 0; i<array.length; i++) {
-  if (i===1){console.log('I am 1 strange loop.');}
-  else {console.log(`I am ${i} strange loops.`);}
+for (var i = 0; i< 25; i++) {
+  if (i===0){array.push('I am 1 strange loop.');}
+  else {array.push(`I am ${i} strange loops.`);}
   
 }
  return array 
